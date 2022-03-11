89a04a98867f0030b524346d9eb5b0cf68e39f5b
Ryan Ashby <rjapsu160@gmail.com>
2022-03-11 09:41:14 -0800

    update Client and Stylist models

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,6 +4,12 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
-    
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
   }
 }
